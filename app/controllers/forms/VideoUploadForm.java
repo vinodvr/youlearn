09bd4f6fa1473747215f8019f005fc10d05d5e7e
vinodvr <vinodvr@flipkart.com>
2015-06-23 03:39:16 +0530

    Added getters/setters

--- app/controllers/forms/VideoUploadForm.java
+++ app/controllers/forms/VideoUploadForm.java
@@ -10,4 +10,12 @@ public class VideoUploadForm {
     @Required
     public String title;
 
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
--- app/models/Video.java
+++ app/models/Video.java
@@ -19,30 +19,7 @@ public class Video {
 
 
     private static Map<String, Video> videos = Maps.newConcurrentMap();
-    static {
-        User user = new User();
-        user.email = "vinodvr@flipkart.com";
-        user.name = "Vinod";
 
-        Video video = new Video();
-        video.id = "2";
-        video.title = "Popular Video";
-        video.totalViews = 10000;
-        video.uploadedAt = new Date();
-        video.uploadedBy = user;
-        video.path = new File("/Users/vinodvr/src/learning/youlearn/public/sample_videos/mov_bbb.mp4");
-
-        Video video1 = new Video();
-        video1.id = "1";
-        video1.title = "First Video";
-        video1.totalViews = 100;
-        video1.uploadedAt = new Date();
-        video1.uploadedBy = user;
-        video1.path = new File("/Users/vinodvr/src/learning/youlearn/public/sample_videos/mov_bbb.mp4");
-
-        videos.put(video.id, video);
-        videos.put(video1.id, video1);
-    }
     public static List<Video> getUserVideos(User user) {
         if (user.email == null) return Collections.EMPTY_LIST;
         List<Video> userVids = Lists.newArrayList();
